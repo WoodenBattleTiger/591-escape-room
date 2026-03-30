0ed3a1ccb62738d544256b7b18203213b5dad82a
WoodenBattleTiger <aguiar.claire@gmail.com>
2026-03-30 14:37:36 -0400

    starting 3d printer work

---
+++ src/map/3d_printer.gd
@@ -0,0 +1,13 @@
+extends Node3D
+
+var isInteractable = true
+var interactableText = "Press \"e\" to use 3D printer"
+
+
+# Called when the node enters the scene tree for the first time.
+func _ready() -> void:
+	pass # Replace with function body.
+
+
+func interact():
+	print("do something here")
