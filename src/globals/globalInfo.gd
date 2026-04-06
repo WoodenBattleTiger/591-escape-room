7a48ec2ea36d7862e2a0669c865cb37ea5338790
Adam Myers <adamjmyers321@gmail.com>
2026-04-06 18:21:38 -0400

    fossils from both dinos spawn proportionally to number of fossils

--- src/globals/globalInfo.gd
+++ src/globals/globalInfo.gd
@@ -30,11 +30,35 @@ var fossil_scenes_triceratops: Array = [
 	"res://src/Items/Fossils/triceratops/tri_skull.tscn",
 ]
 
+var fossil_scenes_tyrannosaur: Array = [
+	"res://src/Items/Fossils/tyrannosaur/_tyran_main_body.tscn",
+	"res://src/Items/Fossils/tyrannosaur/tyran_complete.tscn",
+	"res://src/Items/Fossils/tyrannosaur/tyran_hip_and_pelvis.tscn",
+	"res://src/Items/Fossils/tyrannosaur/tyran_left_arm.tscn",
+	"res://src/Items/Fossils/tyrannosaur/tyran_left_calf.tscn",
+	"res://src/Items/Fossils/tyrannosaur/tyran_left_foot.tscn",
+	"res://src/Items/Fossils/tyrannosaur/tyran_left_shoulder.tscn",
+	"res://src/Items/Fossils/tyrannosaur/tyran_left_thigh.tscn",
+	"res://src/Items/Fossils/tyrannosaur/tyran_right_arm.tscn",
+	"res://src/Items/Fossils/tyrannosaur/tyran_right_foot.tscn",
+	"res://src/Items/Fossils/tyrannosaur/tyran_right_shin.tscn",
+	"res://src/Items/Fossils/tyrannosaur/tyran_right_shoulder.tscn",
+	"res://src/Items/Fossils/tyrannosaur/tyran_right_thigh.tscn",
+	"res://src/Items/Fossils/tyrannosaur/tyran_skull.tscn",
+	"res://src/Items/Fossils/tyrannosaur/tyran_tooth.tscn"
+]
+
 var shuffled_triceratops_scenes: Array = []
+var shuffled_tyrannosaur_scenes: Array = []
 
 func _ready():
 	shuffle_triceratops()
+	shuffle_tyrannosaur()
 
 func shuffle_triceratops():
 	shuffled_triceratops_scenes = fossil_scenes_triceratops.duplicate()
 	shuffled_triceratops_scenes.shuffle()
+	
+func shuffle_tyrannosaur():
+	shuffled_tyrannosaur_scenes = fossil_scenes_tyrannosaur.duplicate()
+	shuffled_tyrannosaur_scenes.shuffle()
